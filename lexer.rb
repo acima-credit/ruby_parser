ebcc5f5b4685e3c2557f8f514360edfda332aa27
David Brady <david.brady@acimacredit.com>
2021-08-03 14:19:29 -0600

    Add comments, and \r to whitespace

--- lexer.rb
+++ lexer.rb
@@ -2,9 +2,28 @@
 
 require 'rly'
 
+# Lexer  ->  Parser  ->  Interpreter
+#  ^^          ^^            ^^ (Compiler)
+# "..."
+
+
+# Raw (Source) Text:
+# "function multiply(x, y) { x * y; }"
+
+# FUNCTION_KEYWORD NAME LEFT_PAREN NAME COMMA NAME RIGHT_PAREN LEFT_BRACE ...
+
+# source text -> Lexer -> tokens -> Parser -> syntax tree -> Interpreter
+
+# Source        | Lexer                             | Parser                           | Interpreter
+# > 3 + 2       | NUMBER("3") PLUS("+") NUMBER("2") | (:+, (:number, 3), (:number, 2)) | 5
+# > (1+1)*3     |
+# > 3*(1-1)     |
+# > abc = 123   |
+# > abc * 2     |
+
 class Lexer < Rly::Lex
   literals "+-*/%=^()"
-  ignore " \t\n"
+  ignore " \t\n\r"
 
   token :NUMBER, /\d+\.?\d*/
   token :NAME, /[a-zA-Z_]\w{2,}/ # variable names must be at least 3 characters
