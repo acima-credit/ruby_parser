8db763a8612fc5c869b723fd99131ac90d9be02a
Tad Thorley <phaedryx@gmail.com>
2021-08-03 15:28:43 -0600

    suggest compose operator

--- lexer.rb
+++ lexer.rb
@@ -10,6 +10,7 @@ class Lexer < Rly::Lex
   token :NAME, /[a-zA-Z_]\w{2,}/ # variable names must be at least 3 characters
   token :FNAME, /ƒ[a-zA-Z_]\w{2,}/ # function name must be at least 3 characters
   token :LAMBDA, /λ/
+  tokem :COMPOSE, /∘/
 
   on_error do |t|
     puts "Illegal character #{t.value}"
