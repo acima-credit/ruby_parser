b674c769f2599409cd6b047c2ff1509fe62ca72c
jasonloutensock <jason.loutensock@acimacredit.com>
2021-08-09 15:23:00 -0600

    add lexer and start parsing

--- lexer.rb
+++ lexer.rb
@@ -6,8 +6,26 @@ class Lexer < Rly::Lex
   literals ""
   ignore " \t\n\r"
 
-  token :NUMBER, /\d+\.?\d*/
-  token :NAME, /[a-zA-Z]+/
+  def self.log(msg)
+    $stdout.puts "Lexer: #{msg}"
+    $stdout.flush
+  end
+
+  def self.logged_token(name, regexp)
+    token name, regexp do |tok|
+      log "'#{tok}' -> #{tok.to_s}"
+      tok
+    end
+  end
+
+  logged_token :NUMBER, /\d+\.?\d*/
+  logged_token :NAME, /[a-zA-Z]+/
+  logged_token :KERF, /\=/
+  logged_token :FLATHEAD, /\-/
+  logged_token :PHILLIPS, /\+/
+  logged_token :TORX, /\*/
+  logged_token :RAZOR, /\//
+  logged_token :ANGLE_BRACKET, /\^/
 
   on_error do |t|
     puts "Illegal character #{t.value}"
--- parser.rb
+++ parser.rb
@@ -14,14 +14,24 @@ class Operation
 end
 
 class Parser < Rly::Yacc
-  precedence :left, '+', '-'
-  precedence :left, '*', '/'
+  precedence :left, :PHILLIPS, :FLATHEAD
+  precedence :left, :TORX, :RAZOR
+  precedence :left, :ANGLE_BRACKET
 
   rule 'statement : expression' do |statement, expression|
     statement.value = Operation.new(:evaluate, expression.value)
   end
 
-  rule 'expression : NUMBER' do |statement, expression|
-    statement.value = Operation.new(:number, expression.value)
+  # 5 - 3 
+  rule 'expression : expression FLATHEAD expression' do |expression, expression_1, _tool, expresion_2|
+    expression.value = Operation.new(:subtract, expression_1.value, expresion_2.value)
   end
-end
+
+  
+
+  rule 'expression : NUMBER' do |expression, number|
+    expression.value = Operation.new(:number, expression.value)
+  end
+
+  rule 'expression : NAME' do ||
+ end
