851d4857e012e9335cfbdd759b55b56a4833eac0
Mike Challis <mike.challis@ryedigital.com>
2021-08-09 20:05:12 +0000

    Start with a nearly blank slate

--- lexer.rb
+++ lexer.rb
@@ -2,31 +2,12 @@
 
 require 'rly'
 
-# Lexer  ->  Parser  ->  Interpreter
-#  ^^          ^^            ^^ (Compiler)
-# "..."
-
-
-# Raw (Source) Text:
-# "function multiply(x, y) { x * y; }"
-
-# FUNCTION_KEYWORD NAME LEFT_PAREN NAME COMMA NAME RIGHT_PAREN LEFT_BRACE ...
-
-# source text -> Lexer -> tokens -> Parser -> syntax tree -> Interpreter
-
-# Source        | Lexer                             | Parser                           | Interpreter
-# > 3 + 2       | NUMBER("3") PLUS("+") NUMBER("2") | (:+, (:number, 3), (:number, 2)) | 5
-# > (1+1)*3     |
-# > 3*(1-1)     |
-# > abc = 123   |
-# > abc * 2     |
-
 class Lexer < Rly::Lex
-  literals "+-*/%=^()"
+  literals ""
   ignore " \t\n\r"
 
   token :NUMBER, /\d+\.?\d*/
-  token :NAME, /[a-zA-Z_]\w{2,}/ # variable names must be at least 3 characters
+  token :NAME, /[a-zA-Z]+/
 
   on_error do |t|
     puts "Illegal character #{t.value}"
--- parser.rb
+++ parser.rb
@@ -15,57 +15,13 @@ end
 
 class Parser < Rly::Yacc
   precedence :left, '+', '-'
-  precedence :left, '*', '/', '%'
-  precedence :left, '^'
-  precedence :right, :UMINUS
-
-  rule 'statement : NAME "=" expression' do |statement, name, _, expression|
-    statement.value = Operation.new(:assign, name.value, expression.value)
-  end
+  precedence :left, '*', '/'
 
   rule 'statement : expression' do |statement, expression|
     statement.value = Operation.new(:evaluate, expression.value)
   end
 
-  rule 'expression : "-" expression %prec UMINUS' do |expression, _neg, exp|
-    expression.value = Operation.new(:negate, exp.value)
-  end
-
-  rule 'expression : "(" expression ")"' do |expression, _lparen, exp, _rparen|
-    expression.value = exp.value
-  end
-
-  rule 'expression : expression "+" expression' do |expression, a, _operation, b|
-    expression.value = Operation.new(:+, a.value, b.value)
-  end
-
-  rule 'expression : expression "-" expression' do |expression, a, _operation, b|
-    expression.value = Operation.new(:-, a.value, b.value)
-  end
-
-  rule 'expression : expression "*" expression' do |expression, a, _operation, b|
-    expression.value = Operation.new(:*, a.value, b.value)
-  end
-
-  rule 'expression : expression "/" expression' do |expression, a, _operation, b|
-    expression.value = Operation.new(:/, a.value, b.value)
-  end
-
-  rule 'expression : expression "%" expression' do |expression, a, _operation, b|
-    expression.value = Operation.new(:%, a.value, b.value)
-  end
-
-  rule 'expression : expression "^" expression' do |expression, a, _operation, b|
-    expression.value = Operation.new(:^, a.value, b.value)
-  end
-
-  rule 'expression : NAME' do |expression, name|
-    exit(0) if %w[quit exit].include?(name.value)
-
-    expression.value = Operation.new(:lookup, name.value)
-  end
-
-  rule 'expression : NUMBER' do |expression, number|
-    expression.value = Operation.new(:number, number.value.to_i)
+  rule 'expression : NUMBER' do |statement, expression|
+    statement.value = Operation.new(:number, expression.value)
   end
 end
