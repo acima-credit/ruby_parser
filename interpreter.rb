26fe1203d5c70eee5d03192fa3a157730c3b7c2b
eddylopez767 <eddy.lopez@acimacredit.com>
2021-08-10 15:20:26 -0600

    adding a + expression
    adding an expression

--- interpreter.rb
+++ interpreter.rb
@@ -7,6 +7,7 @@ class Interpreter
     case tree.operation
     when :evaluate then evaluate(*tree.arguments)
     when :number then number(*tree.arguments)
+    when :+ then add(*tree.arguments)  
     else
       puts "I don't know how to handle operation '#{tree.operation}'!"
     end
@@ -15,4 +16,8 @@ class Interpreter
   def number(number)
     number
   end
+
+  def add(value1, value2)
+    evaluate(value1) + evaluate(value2)
+  end 
 end
--- parser.rb
+++ parser.rb
@@ -24,4 +24,8 @@ class Parser < Rly::Yacc
   rule 'expression : NUMBER' do |statement, expression|
     statement.value = Operation.new(:number, expression.value)
   end
+
+  rule 'expression : expression "+" expression' do |expression, value1, operation, value2|
+    expression.value = Operation.new(:+, value1.value, value2.value)
+  end
 end
