43a6609fdca111c2bef34d1f8bb665418666ea0c
jasonloutensock <jason.loutensock@acimacredit.com>
2021-08-10 15:28:18 -0600

    add more logging and finish arithmetic operators

--- lexer.rb
+++ lexer.rb
@@ -1,31 +1,36 @@
 # frozen_string_literal: true
 
 require 'rly'
-
+require 'colorize'
 class Lexer < Rly::Lex
   literals ""
   ignore " \t\n\r"
 
   def self.log(msg)
-    $stdout.puts "Lexer: #{msg}"
+    $stdout.puts "Lexer: #{msg}".black.on_green
     $stdout.flush
+  end
+  def log(msg)
+    self.class.log(msg)
   end
 
   def self.logged_token(name, regexp)
     token name, regexp do |tok|
-      log "'#{tok}' -> #{tok.to_s}"
+      log "'#{tok}' --> #{tok.to_s}"
       tok
     end
   end
 
   logged_token :NUMBER, /\d+\.?\d*/
+  logged_token :CLOSE_TOOLBOX, /exit/
   logged_token :NAME, /[a-zA-Z]+/
   logged_token :KERF, /\=/
   logged_token :FLATHEAD, /\-/
   logged_token :PHILLIPS, /\+/
   logged_token :TORX, /\*/
   logged_token :RAZOR, /\//
-  logged_token :ANGLE_BRACKET, /\^/
+  logged_token :SQUARE, /\^/
+
 
   on_error do |t|
     puts "Illegal character #{t.value}"
--- parser.rb
+++ parser.rb
@@ -14,24 +14,74 @@ class Operation
 end
 
 class Parser < Rly::Yacc
+
+  def self.log(msg)
+    $stdout.puts "Parser: #{msg}".black.on_light_blue
+    $stdout.flush
+  end
+
+  def log(msg)
+    self.class.log(msg)
+  end
+
   precedence :left, :PHILLIPS, :FLATHEAD
   precedence :left, :TORX, :RAZOR
-  precedence :left, :ANGLE_BRACKET
+  precedence :left, :SQUARE
+
+  # Exit by sending to interpreter, ignoring _close param
+  rule 'statement : CLOSE_TOOLBOX' do |statement, _close|
+    log "statement : CLOSE_TOOLBOX --> #{_close.value}"
+    statement.value = Operation.new(:exit)
+  end
 
   rule 'statement : expression' do |statement, expression|
+    log "statement : expression --> #{expression.value}"
     statement.value = Operation.new(:evaluate, expression.value)
   end
 
-  # 5 - 3 
+  # Assignment x = y   Intentionally ignoring _tool parameter
+  rule 'expression : NAME KERF expression' do |expression, expression_1, _tool, expresion_2|
+    log "expression : NAME KERF expression --> (:assign, #{expression_1.value}, #{expresion_2.value})"
+    expression.value = Operation.new(:assign, expression_1.value, expresion_2.value)
+  end
+
+  # Subtraction x - y   Intentionally ignoring _tool parameter
   rule 'expression : expression FLATHEAD expression' do |expression, expression_1, _tool, expresion_2|
+    log "expression : expression FLATHEAD expression --> (:subtract, #{expression_1.value}, #{expresion_2.value})"
     expression.value = Operation.new(:subtract, expression_1.value, expresion_2.value)
   end
 
-  
-
-  rule 'expression : NUMBER' do |expression, number|
-    expression.value = Operation.new(:number, expression.value)
+  # Addition x + y  Intentionally ignoring _tool parameter
+  rule 'expression : expression PHILLIPS expression' do |expression, expression_1, _tool, expresion_2|
+    log "expression : expression PHILLIPS expression --> (:add #{expression_1.value}, #{expresion_2.value})"
+    expression.value = Operation.new(:add, expression_1.value, expresion_2.value)
   end
 
-  rule 'expression : NAME' do ||
- end
+  # Multiplication x * y Intentionally ignoring _tool parameter
+  rule 'expression : expression TORX expression' do |expression, expression_1, _tool, expresion_2|
+    log "expression : expression TORX expression --> (:multiply, #{expression_1.value}, #{expresion_2.value})"
+    expression.value = Operation.new(:multiply, expression_1.value, expresion_2.value)
+  end
+
+  # Division x / y Intentionally ignoring _tool parameter
+  rule 'expression : expression RAZOR expression' do |expression, expression_1, _tool, expresion_2|
+    log "expression : expression RAZOR expression --> (:divide #{expression_1.value}, #{expresion_2.value})"
+    expression.value = Operation.new(:divide, expression_1.value, expresion_2.value)
+  end
+
+  # Exponentiation x ^ y Intentionally ignoring _tool parameter
+  rule 'expression : expression SQUARE expression' do |expression, expression_1, _tool, expresion_2|
+    log "expression : expression SQUARE expression --> (:raise, #{expression_1.value}, #{expresion_2.value})"
+    expression.value = Operation.new(:raise, expression_1.value, expresion_2.value)
+  end
+
+  rule 'expression : NUMBER' do |expression, number|
+    log "expression : NUMBER --> #{number.value}"
+    expression.value = Operation.new(:number, number.value)
+  end
+
+  rule 'expression : NAME' do |expression, name|
+    log "expression : NAME --> #{name.inspect}"
+    expression.value = Operation.new(:name, name.value)
+  end
+end
